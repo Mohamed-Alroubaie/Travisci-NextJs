b53aa57fceb03c0e3c14deb59b5658538a2e75e2
Mohamed <me.com>
2021-01-08 07:07:06 +0100

    Initialized

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Travis Ci</title>
+      </Head>
+      <div>
+        <h1>Travis Ci</h1>
+      </div>
+    </div>
+  );
+}
